d64a5f3cec8a217f3012dc96b8ea6a62efc433eb
Atima <atima.ng@hotmail.ca>
2020-09-24 16:10:57 -0400

    E1: Added circle and square shapes

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -19,6 +19,22 @@ let circle1 = {
   }
 }
 
+let circle2 = {
+  x: 750,
+  y: 400,
+  size: 100,
+  speed:1,
+}
+
+let square1 = {
+  size: 50,
+  //fill: {
+  //  r:237,
+  //  g:177,
+  //  b:28,
+  //}
+}
+
 // setup()
 //
 // Description of setup() goes here.
@@ -34,11 +50,23 @@ function draw() {
 
   background(backgroundShade);
 
-  //Circle1 movement
+  //Drawing circle1 movement
+
   //circle1.x = circle1.x + circle.speed;
   //circle1.x = constrain(circle.x, 0, width/2);
   //circle1.size = circle1.size + circle.growthRate;
   //circle.size = constrain(circle.size, 0, width);
-  fill(circle1.fill.r, circle1.fill.g,circle1.fill.b,);
+  fill(circle1.fill.r, circle1.fill.g,circle1.fill.b);
   ellipse(circle1.x, circle1.y, circle1.size);
+
+  // Drawing circle2 growing.
+  circle2.size += circle2.speed
+  circle2.size = constrain(circle2.size, 0, 200);
+  ellipse(circle2.x, circle2.y, circle2.size);
+
+// Drawing growing square.
+//fill(square.fill.r, square.fill.g, square.fill.b);
+  rectMode(CENTER);
+  square(mouseX, mouseY, square1.size);
+
 }
