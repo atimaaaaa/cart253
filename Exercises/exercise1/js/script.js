7be91c02062a43925af864f90ecb9409d49cf585
Atima <atima.ng@hotmail.ca>
2020-09-24 15:35:09 -0400

    E1: Created canvas, created first blue shape

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -1,15 +1,30 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+"I like to move it move it!" Exercise
+Atima Ng
 
-Here is a description of this template p5 project.
+An exercise on practicing using map() and constrain()
 **************************************************/
+let backgroundShade = 100;
+
+let circle1 = {
+  x: 250,
+  y: 400,
+  //speed: 1,
+  size: 100,
+  //growthRate: 0.5,
+  fill: {
+    r:38,
+    g:203,
+    b:224,
+  }
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  
+  createCanvas(1000,800);
+  noStroke();
 }
 
 // draw()
@@ -17,4 +32,13 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
 
+  background(backgroundShade);
+
+  //Circle1 movement
+  //circle1.x = circle1.x + circle.speed;
+  //circle1.x = constrain(circle.x, 0, width/2);
+  //circle1.size = circle1.size + circle.growthRate;
+  //circle.size = constrain(circle.size, 0, width);
+  fill(circle1.fill.r, circle1.fill.g,circle1.fill.b,);
+  ellipse(circle1.x, circle1.y, circle1.size);
 }
