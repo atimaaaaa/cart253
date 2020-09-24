61392f48d3d28f3f1360609d3ac5ddb1102387ce
Atima <atima.ng@hotmail.ca>
2020-09-23 23:46:32 -0400

    Learned map and constraint!

--- Experiments/variables-experiments/js/script.js
+++ Experiments/variables-experiments/js/script.js
@@ -7,17 +7,17 @@ Here is a description of this template p5 project.
 
 let backgroundShade = 0;
 let circle = {
-  x: 250,
+  x: 0,
   y: 250,
   size: 100,
   speed: 1,
-  fill: 0
+  fill: 255
 }
 
 // setup()
 //
 // Description of setup() goes here.
-function setup(){
+function setup() {
   createCanvas(500,500);
 }
 
@@ -27,14 +27,10 @@ function setup(){
 function draw(){
   background(backgroundShade);
 
-  circle.speed = random(-5, 5);
   circle.x = circle.x + circle.speed;
-  //circle.y = random(0,height);
-  circle.size = random(10,100);
+  circle.x = constrain(circle.x,0,width);
 
-  circle.fill = random(0,255) ;
+  circle.fill = map(circle.x,0,width,0,255);
   fill(circle.fill);
   ellipse(circle.x, circle.y, circle.size);
-
-
 }
