ed230943566858872aed5e976ed8e4e3de962fb6
Atima <atima.ng@hotmail.ca>
2020-09-24 13:28:37 -0400

    A3: Completed activity
    sorry i didn't commit more oftnen

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -8,13 +8,58 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0
+};
+
+let circle1 = {
+  x:0,
+  y: 250,
+  size: 100,
+  fill: 255,
+  alpha: 200,
+  speed: 1,
+  growthRate: 1;
+};
+let circle2 = {
+  x: 500,
+  y: 250,
+  size: 75,
+  fill: 255,
+  alpha: 200,
+  speed: -1,
+  ratio: 0.75,
+};
+
+
 function setup() {
-   createCanvas(1000,700);
+   createCanvas(500,500);
+   noStroke();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  //Background
+  background(bg.r, bg.g, bg.b);
+  bg.r = map(circle1.size,100,width,0,255);
+
+  // Left circle
+  circle1.x = circle1.x + circle1.speed;
+  circle1.x = constrain(circle1.x, 0, width/2);
+  circle1.size = circle1.size + circle1.growthRate;
+  circle1.size = constrain(circle1.size,0,width);
+  fill(circle1.fill, circle1.alpha);
+  ellipse(circle1.x, circle1.y, circle1.size);
+
+  // Right circle
+  circle2.x = circle2.x + circle2.speed;
+  circle2.x = constrain(circle2.x, width/2, width);
+  circle2.size = circle1.size * circle2.ratio;
+  fill(circle2.fill, circle2.alpha);
+  ellipse(circle2.x, circle2.y, circle2.size);
 
 }
