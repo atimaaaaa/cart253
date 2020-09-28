e3cd1103c0f046393607d88491d1c7f790c09296
Atima <atima.ng@hotmail.ca>
2020-09-28 17:43:35 -0400

    Experimented with two conditionals

--- Experiments/conditionals_experiments/js/script.js
+++ Experiments/conditionals_experiments/js/script.js
@@ -8,13 +8,42 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
+let backgroundShade = 0;
+
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 10
+}
+
 function setup() {
-  
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgroundShade);
 
+  circle.x = circle.x + circle.speed;
+
+  if (circle.x > width) {
+    circle.speed = -circle.speed;
+  }
+
+  if (circle.x < 0) {
+    circle.speed = -circle.speed;
+  }
+
+  if (mouseY < height/2){
+    fill(255,0,0);
+  }
+
+  if (mouseY > height/2){
+    fill(0,0,255);
+  }
+
+  ellipse(circle.x, circle.y, circle.size);
 }
