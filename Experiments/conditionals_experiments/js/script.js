f5a04bc699628152b6494d91c16ac1ca15452b5a
Atima <atima.ng@hotmail.ca>
2020-09-29 17:14:35 -0400

    experimented with velocity! very fun stuff

--- Experiments/conditionals_experiments/js/script.js
+++ Experiments/conditionals_experiments/js/script.js
@@ -1,13 +1,10 @@
-let bg = {
-  r:0,
-  g:0,
-  b:0
-}
-
 let circle = {
   x: 250,
   y: 250,
-  size:100
+  size:100,
+  vx: 2,
+  vy: 0,
+  speed: 2
 }
 
 function setup() {
@@ -15,13 +12,26 @@ function setup() {
 }
 
 function draw(){
-  background(bg.r, bg.g, bg.b);
+  background(0);
 
-  ellipse(circle.x,circle.y, circle.size);
-}
+  if (mouseX < circle.x){
+    circle.vx = -circle.speed;
+  }
 
-function mousePressed(){
-  bg.r = random(0,255);
-  bg.g = random(0,255);
-  bg.b = random (0,255);
+  else {
+    circle.vx = circle.speed;
+  }
+
+  if (mouseY < circle.y){
+    circle.vy = -circle.speed;
+  }
+
+  else {
+    circle.vy = circle.speed;
+  }
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
 }
