b73e28b5ecd99d4343a6d4b65130f8b993b30517
Atima <atima.ng@hotmail.ca>
2020-09-29 16:16:52 -0400

    experimented with mouse inputs

--- Experiments/conditionals_experiments/js/script.js
+++ Experiments/conditionals_experiments/js/script.js
@@ -1,58 +1,27 @@
-let caterpillar = {
-  x: 100,
+let bg = {
+  r:0,
+  g:0,
+  b:0
+}
+
+let circle = {
+  x: 250,
   y: 250,
-  segmentSize: 50
+  size:100
 }
 
 function setup() {
   createCanvas(500,500);
 }
 
-function draw() {
-  background(0);
-  noStroke();
-  fill(100,200,100);
+function draw(){
+  background(bg.r, bg.g, bg.b);
 
-  // let x = caterpillar.x; // Where will I sart drawing?
-  // let numSegment = 10; // How many segments to draw?
-  // let segmentsDrawn = 0; // How many have been drawn?
-  //
-  // while(segmentsDrawn< numSegment){
-  //   ellipse(x, caterpillar.y, caterpillar.segmentSize);
-  //   x = x + 40;
-  //   segmentsDrawn = segmentsDrawn + 1;
-  // }
-
-  let x = caterpillar.x;
-  let numSegments = 10;
-
-  for (let segmentsDrawn = 0; segmentsDrawn < numSegments; segmentsDrawn++){
-        ellipse(x, caterpillar.y, caterpillar.segmentSize);
-        x = x + 30;
-      }
-  }
-
-We need to know how many stars we want to draw in the sky
-let numStars = 200;
-
-function setup() {
-  createCanvas(500, 500);
-  // White stroke because we're using point() on black
-  stroke(255);
+  ellipse(circle.x,circle.y, circle.size);
 }
 
-function draw() {
-  // Black sky
-  background(0);
-  // randomSeed() lets us make random() predictable: it will generate the same sequence of numbers
-  // each time draw() is called
-  randomSeed(0);
-  // Our for loop counts from 0 to numStars
-  for (let i = 0; i < numStars; i++) {
-    // Choose a random x an0d y position on the canvas
-    let x = random(0, width);
-    let y = random(0, height);
-    // Draw a point (star) there
-    point(x, y);
-  }
+function mousePressed(){
+  bg.r = random(0,255);
+  bg.g = random(0,255);
+  bg.b = random (0,255);
 }
