a5079519e0a4098c033baaf2391abcace1811b8a
Atima <atima.ng@hotmail.ca>
2020-09-30 15:59:15 -0400

    experimented with debugging conditionals

--- Experiments/conditionals_experiments/js/script.js
+++ Experiments/conditionals_experiments/js/script.js
@@ -1,28 +1,24 @@
-let clown = {
-  x: 250,
-  y: 250,
-  size: 100,
-  image: undefined
-}
-
-function preload() {
-  clown.image = loadImage("assets/images/clown.png");
+let circle = {
+  x:250,
+  y:250,
+  size:100
 }
 
 function setup(){
   createCanvas(500,500);
 }
 
-function draw() {
+function draw(){
   background(0);
 
-  clown.x = mouseX;
-  clown.y = mouseY;
-
-  imageMode(CENTER);
-  image(clown.image, clown.x, clown.y, clown.size, clown.size);
+  if (mouseX < width/2) {
+    console.log("Mouse is to the left...");
+    fill(255,0,0);
+  }
+  else {
+    console.log("Mouse is to the right...");
+    fill(0,255,0);
 }
 
-function mousePressed() {
-  clown.size = clown.size + 50;
+ellipse(circle.x,circle.y, circle.size);
 }
