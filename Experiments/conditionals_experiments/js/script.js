e642b69c379efa2d479e949a76e16e8a12589f39
Atima <atima.ng@hotmail.ca>
2020-09-30 14:20:32 -0400

    experimented with time

--- Experiments/conditionals_experiments/js/script.js
+++ Experiments/conditionals_experiments/js/script.js
@@ -1,17 +1,28 @@
-let clownImage;
-
-function preload() {
-  clownImage = loadImage("assets/images/clown.png");
+let clown = {
+  x: 250,
+  y: 250,
+  size: 100,
+  image: undefined
 }
 
-function setup() {
-  createCanvas(500,500);
+function preload() {
+  clown.image = loadImage("assets/images/clown.png");
+}
 
+function setup(){
+  createCanvas(500,500);
 }
 
 function draw() {
   background(0);
 
+  clown.x = mouseX;
+  clown.y = mouseY;
+
   imageMode(CENTER);
-  image(clownImage,mouseX,mouseY,400,10);
+  image(clown.image, clown.x, clown.y, clown.size, clown.size);
+}
+
+function mousePressed() {
+  clown.size = clown.size + 50;
 }
