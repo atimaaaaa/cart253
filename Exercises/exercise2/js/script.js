bd55506612e99321481d3da974bbb8b827de61c9
Atima <atima.ng@hotmail.ca>
2020-10-01 17:57:06 -0400

    E2: Added tube and conditional

--- Exercises/exercise2/js/script.js
+++ Exercises/exercise2/js/script.js
@@ -5,10 +5,7 @@ Atima Ng
 Here is a description of this template p5 project.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
- let thomp = {
+let thomp = {
    x:250,
    y:0,
    size:100,
@@ -18,39 +15,59 @@ Here is a description of this template p5 project.
    image: undefined
 }
 
-  let coin = {
-    x:250,
-    y:0,
-    size:100,
-    vx:0,
-    vy:0,
-    speed: 1,
-    image: undefined
-  }
+let coin = {
+  x:250,
+  y:0,
+  size:100,
+  vx:0,
+  vy:0,
+  speed: 1,
+  image: undefined
+}
 
 let user = {
   x:250,
   y:250,
   size: 100,
-  image: undefined,
+  image: undefined
 }
 
+let tube = {
+  x: 600,
+  y: 750,
+  size:100,
+  image: undefined,
+  bg: {
+    r:0,
+    g:255,
+    b:0
+  }
+}
+
+// preload()
+//
+// These will be the images loaded into the exercise.
 function preload(){
   user.image = loadImage("assets/images/mario.png");
   thomp.image = loadImage("assets/images/thomp.png");
   coin.image = loadImage("assets/images/coin.png");
+  tube.image = loadImage("assets/images/tube.png");
 }
 
+// setup()
+//
+// This is where initial positions of images will be placed.
 function setup() {
   createCanvas(800,800);
 
-  //Coin Position
+  //Thomp Position
   thomp.x = random(0, width);
   thomp.vy = thomp.speed;
 
-  //Coin Positio
+  //Coin Position
   coin.x = random(0, width);
-  coin.vy = coin.speed;
+  coin.y = random(0, 700);
+
 
   noCursor();
 }
@@ -70,17 +87,6 @@ function draw() {
     thomp.x = random(0, width);
   }
 
-  //Coin movement
-  for (let i = 0; i < 10; i++){
-    coin.x = coin.x + coin.vx;
-    coin.y = coin.y + coin.vy;
-    }
-
-  if (coin.y > height) {
-    coin.y = 0;
-    coin.x = random(0,height);
-  }
-
   // user movement
   user.x = mouseX;
   user.y = mouseY;
@@ -89,22 +95,30 @@ function draw() {
   let d = dist (thomp.x, thomp.y, user.x, user.y);
   if (d < thomp.size/2 + user.size/3) {
     noLoop();
-    background(255,0,0);
+    background(255,0,0); // Red background
   }
 
   // Action when user touches the coin.
-  let dcoin = dist (coin.x, coin.y, user.x, user.y);
-  let bg = {
-    r: random(0,255),
-    g: random(0,255),
-    b: random(0,255),
-  }
+  let dCoin = dist (coin.x, coin.y, user.x, user.y);
+  // let bg = {
+  //   r: random(0,255),
+  //   g: random(0,255),
+  //   b: random(0,255),
+  // }
 
-  if (dcoin < coin.size/2 + user.size/2) {
-    coin.x = random(0,width);
-    coin.y = 0; // Coin goes back on top at a random x position
-    background(bg.r, bg.g,bg.b);
+  if (dCoin < coin.size/2 + user.size/2 && mouseIsPressed) {
+    coin.x = mouseX;
+    coin.y = mouseY;
     }
+
+
+  // Action when coin touches the tube.
+  let dTube = dist (coin.x, coin.y, tube.x, tube.y);
+  if (dTube < tube.size/2 + coin.size/2) {
+    coin.x = random(0, width);
+    coin.y = random(0, 700);
+    background(tube.bg.r, tube.bg.g, tube.bg.b);
+  }
 
   // Displaying thomp character.
   imageMode(CENTER);
@@ -115,7 +129,10 @@ function draw() {
   imageMode(CENTER);
   image(coin.image, coin.x, coin.y);
 
+  // Displaying tube
+    image(tube.image, tube.x, tube.y);
+
 // Displaying user
   imageMode(CENTER);
-  image(user.image, user.x, user.y, 50,50);
+  image(user.image, user.x, user.y,50,50);
 }
