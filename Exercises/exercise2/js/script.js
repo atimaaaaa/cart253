a16a14768d0dbf8da2a0a2321171f802b44ae9ab
Atima <atima.ng@hotmail.ca>
2020-10-01 15:30:21 -0400

    E2: Defined variables

--- Exercises/exercise2/js/script.js
+++ Exercises/exercise2/js/script.js
@@ -1,6 +1,6 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Exercise 2: Dodge-Em
+Atima Ng
 
 Here is a description of this template p5 project.
 **************************************************/
@@ -8,8 +8,22 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
+let thomp = {
+  x:250,
+  y:0,
+  size:100,
+  vx:0,
+  vy:0,
+  speed:10,
+}
+
 function setup() {
-  
+  createCanvas(800,800);
+
+  thomp.x = random (0, width);
+  thomp.vx = thomp.speed;
+
+  noCursor();
 }
 
 // draw()
