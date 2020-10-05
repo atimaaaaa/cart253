6f6e263fc36cf610c79feb246964bb27a4498b1b
Atima <atima.ng@hotmail.ca>
2020-10-05 19:37:22 -0400

    E5: experimented with defining my own functions

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,20 +1,47 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+let circle = {
+  x:0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0
+};
 
-Here is a description of this template p5 project.
-**************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-  
+  createCanvas(500,500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
 
+function draw() {
+  background (0);
+
+  move();
+  wrap();
+  display();
+}
+
+function move() {
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+}
+
+function wrap(){
+  if (circle.x > width) {
+    reset();
+  }
+}
+
+function display(){
+  fill(255,0,0);
+  ellipse(circle.x, circle.y, circle.size);
+}
+
+function reset(){
+  circle.x = 0;
+  circle.vx = circle.vx + 2;
+  circle.vy = circle.vy - 0.25;
+  circle.size = circle.size + 5;
+}
+
+function mousePressed() {
+  reset();
 }
