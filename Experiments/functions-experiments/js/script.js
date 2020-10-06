45e1885b7b5c314a254c726e13f83932076cb560
Atima <atima.ng@hotmail.ca>
2020-10-06 01:05:24 -0400

    E5: Experimented with typography

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,20 +1,29 @@
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(0);
+  background(127);
 
-  let color = randomColor();
-  //fill(color.r, color.g, color.b);
-  ellipse(250,250,100,100);
-}
+  textAlign(CENTER, CENTER);
+  // Make the font size respond to the mouse
+  let size = map(mouseX, 0, width, 12, 128);
+  textSize(size);
+  textStyle(BOLD);
 
-function randomColor() {
-  let result = {
-    r: random(0,255),
-    g: random(0,255),
-    b: random(0,255)
-  }
-  return result;
+  // Make the fill respond to the mouse
+  let red = map(mouseX, 0, width, 100, 200);
+  let green = map(mouseY, 0, height, 100, 200);
+  let blue = map(mouseX + mouseY, 0, width + height, 100, 200);
+  fill(red, green, blue);
+
+  // Make the stroke color respond to the mouse
+  let strokeShade = map(mouseX, 0, width, 0, 255);
+  stroke(strokeShade);
+
+  // Make the stroke weight respond to the mouse
+  let weight = map(mouseY, 0, height, 0, 40);
+  strokeWeight(weight);
+
+  text(`Hello, World!`, 250, 250);
 }
