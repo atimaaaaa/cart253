74dd626c16aca3111665d9cd190a2eb2795290d2
Atima <atima.ng@hotmail.ca>
2020-10-05 22:05:56 -0400

    E5: Experimented with functions with parameters

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,47 +1,20 @@
-let circle = {
-  x:0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0
-};
-
-function setup() {
+function setup(){
   createCanvas(500,500);
 }
 
+function draw(){
+  background(0);
 
-function draw() {
-  background (0);
-
-  move();
-  wrap();
-  display();
+  parallels(100,300,30,100,3,5);
+  parallels(300,300,30,100,3,5);
 }
 
-function move() {
-  circle.x = circle.x + circle.vx;
-  circle.y = circle.y + circle.vy;
-}
-
-function wrap(){
-  if (circle.x > width) {
-    reset();
+function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing){
+  for(let i = 0; i < numLines; i++){
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x,y,lineWidth,lineHeight);
+    y = y + lineSpacing;
   }
-}
-
-function display(){
-  fill(255,0,0);
-  ellipse(circle.x, circle.y, circle.size);
-}
-
-function reset(){
-  circle.x = 0;
-  circle.vx = circle.vx + 2;
-  circle.vy = circle.vy - 0.25;
-  circle.size = circle.size + 5;
-}
-
-function mousePressed() {
-  reset();
 }
