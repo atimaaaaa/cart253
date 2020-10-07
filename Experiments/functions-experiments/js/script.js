aecbbd228bcf1bae45d8c3561fb1ac2511a88a6b
Atima <atima.ng@hotmail.ca>
2020-10-07 14:52:44 -0400

    experimented with keyboard inputs

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,64 +1,84 @@
+//
+//
+// let bg = 0;
+//
+// function setup() {
+//   createCanvas(500,500);
+//
+// }
+//
+// function draw(){
+//   background(bg);
+//
+//   textAlign(CENTER, CENTER);
+//   textSize(64);
+//   fill(255);
+//   text(keyCode, width/2,height/2);
+//
+// }
+//
+// function keyPressed(){
+//     if (keyCode === UP_ARROW) {
+//       bg = bg + 10;
+//       bg = constrain (bg, 0,255);
+//     }
+//     else if(keyCode === DOWN_ARROW) {
+//       bg = bg - 10;
+//       bg = constrain(bg,0,255);
+//     }
+// }
 
 
-let circle= {
-  x:0,
+
+let circle = {
+  x:250,
   y:250,
   size:100,
   vx:0,
   vy:0,
-  speed: 2
+  speed:10
 };
-
-let state = `title`; // title, animation, ending
 
 function setup() {
   createCanvas(500,500);
-  circle.vx = circle.speed;
-  textSize(32);
-  textAlign(CENTER, CENTER);
 }
 
-function draw() {
+function draw(){
   background(0);
 
-    if (state === `title`) {
-      title();
-     }
-     else if (state === `animation`) {
-       animation();
-     }
-     else if (state === `ending`) {
-        ending();
-     }
-}
-
-
-function title() {
-  // title
-   fill(255);
-   text(`hello, world!`, width/2, height/2);
-}
-
-function animation() {
-  // Animation
-  circle.x = circle.x + circle.vx;
-  circle.y = circle.y + circle.vy;
-
-  if (circle.x > width) {
-    state = `ending`;
+  handleImput();
+  move();
+  display();
   }
 
-  ellipse(circle.x, circle.y, circle.size);
-}
+function handleImput() {
+   if (keyIsDown(LEFT_ARROW)) {
+     circle.vx = -circle.speed;
 
-function ending(){
-  //Ending
-  fill(120);
-  text(`Life is hard`, 250, 250);
-}
+   }
+   else if(keyIsDown(RIGHT_ARROW)) {
+     circle.vx = circle.speed;
+   }
+   else {
+     circle.vx = 0;
+   }
+   if (keyIsDown(UP_ARROW)) {
+     circle.vy = -circle.speed;
+   }
+   else if (keyIsDown(DOWN_ARROW)) {
+     circle.vy = circle.speed;
+   }
+   else {
+     circle.vy = 0;
+   }
+ }
 
-function keyPressed(){
-  if (state === `title`){
-    state = `animation`;
-  }
-}
+ function move(){
+   circle.x = circle.x + circle.vx;
+   circle.y = circle.y + circle.vy;
+ }
+
+ function display() {
+   fill(150);
+   ellipse(circle.x, circle.y, circle.size);
+ }
