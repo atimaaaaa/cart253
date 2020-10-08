bb2b6e19e96d9202cf5b0d2042dad9076a6af088
Atima <atima.ng@hotmail.ca>
2020-10-08 18:13:27 -0400

    P1: Added falling circles (cats)

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -13,29 +13,35 @@ let base = {
 }
 
 let cat1 = {
-  x:0,
+  x: 0,
   y:0,
   size:100,
   vx:0,
-  vy:0
+  vy:2
 }
 
 let cat2 = {
-  x:0,
-  y:0,
+  x:250,
+  y: -200,
   size:100,
   vx:0,
-  vy:0
+  vy: 2
 }
 
-let catcher = base;
+let cat3 = {
+  x:250,
+  y: -300,
+  size:100,
+  vx:0,
+  vy: 2
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(1000,1000);
-
+  base.y = height - 200;
 }
 
 // draw()
@@ -43,16 +49,30 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+  base.x = mouseX;
 
+  moveCat(cat1);
+  moveCat(cat2);
+  moveCat(cat3);
+  checkCatch();
+  display();
+}
 
-// Display base and cats.
+function moveCat(cat) {
+  // Movement of cats falling down.
+  cat.x = 300;
+  cat.y = cat.y + cat.vy;
+}
 
+function checkCatch(){
 
+}
 
-  cat1.x = mouseX;
-  cat1.y = height - 150;
+function display(){
+  // Display base and cats.
+
   ellipse(cat1.x, cat1.y, cat1.size);
-
-
-
+  ellipse(cat2.x, cat2.y, cat2.size);
+  ellipse(cat3.x, cat3.y, cat3.size);
+  ellipse(base.x, base.y, base.size);
 }
