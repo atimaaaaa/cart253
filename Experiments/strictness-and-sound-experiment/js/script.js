4ab16fd3fcb7e21b5c86e835fb8950c0605cd123
Atima <atima.ng@hotmail.ca>
2020-10-10 18:49:51 -0400

    E5: experimented ith sounds

--- Experiments/strictness-and-sound-experiment/js/script.js
+++ Experiments/strictness-and-sound-experiment/js/script.js
@@ -8,13 +8,83 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
-   createCanvas(1000,700);
+let music;
+
+let clownImage = {
+  x: -600,
+  y: 350,
+  vx: 2,
+  vy: 0,
+  size:300,
+  image: undefined,
+};
+
+let bg = {
+  r:0,
+  g:0,
+  b:0
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+let state = `title`;
 
+function preload() {
+  music = loadSound(`assets/sounds/Never Gonna Give You Up Original.mp3`);
+  clownImage.image = loadImage(`assets/images/clown.png`);
+}
+
+function setup() {
+   createCanvas(1000,700);
+   bg.r = random(0,255);
+   bg.g = random(0,255);
+   bg.b = random(0,255);
+
+}
+
+function draw() {
+  background(0);
+
+  if(mousePressed() || keyPressed()) {
+    clownMovement();
+  }
+
+
+}
+
+
+function mousePressed() {
+
+
+  tryMusic();
+  bgFlashing();
+  clownMovement()
+}
+
+function keyPressed() {
+
+
+  tryMusic();
+  bgFlashing();
+  clownMovement()
+
+}
+
+function tryMusic() {
+  if(!music.isPlaying()) {
+    music.loop();
+  }
+}
+
+function clownMovement() {
+  bgFlashing();
+  clownImage.x = clownImage.x + clownImage.vx;
+  imageMode(CENTER);
+  image(clownImage.image, clownImage.x, clownImage.y, clownImage.size,clownImage.size);
+}
+
+function bgFlashing() {
+  bg.r = random(0,255);
+  bg.g = random(0,255);
+  bg.b = random(0,255);
+
+  background(bg.r, bg.g,bg.b);
 }
