8ec378750e4f9fc1cdf8229042f020b9c6358c04
Atima <atima.ng@hotmail.ca>
2020-10-22 17:46:54 -0400

    E4: Started exercise4

--- Exercises/exercise4/js/script.js
+++ Exercises/exercise4/js/script.js
@@ -1,20 +1,74 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 4: Age of aquariums
+Atima Ng
 
 Here is a description of this template p5 project.
 **************************************************/
+"use strict";
+
+let school = []; // Empty array with the fish
+let schoolSize = 10;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  
+  createCanvas(600, 600);
+
+  for (let i = 0; i < schoolSize; i++) {
+    school[i] = createFish(random(0, width), random(0, height));
+  }
+}
+
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
 }
 
 // draw()
 //
 // Description of draw() goes here.
-function draw() {
+function draw(fish) {
+  background(0);
 
+  for (let i = 0; i < school.length; i++) {
+    moveFish(school[i]);
+    displayFish(school[i]);
+  }
+}
+
+function moveFish(fish) {
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+function displayFish(fish) {
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
+}
+
+function mousePressed() {
+  let fish = createFish(mouseX, mouseY);
+  school.push(fish);
 }
