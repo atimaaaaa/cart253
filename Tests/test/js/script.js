f6d110d2fafb37388edbdcdc926fdd9460e0d34a
Atima <atima.ng@hotmail.ca>
2020-10-29 17:42:37 -0400

    test: update

--- Tests/test/js/script.js
+++ Tests/test/js/script.js
@@ -1,68 +1,10 @@
-"use strict";
-
-// Garden with red flowers
-let redGarden = [];
-let redGardenSize = 0;
-
-// setup() creates the canvas and the flowers in the garden
 function setup() {
-  createCanvas(600, 600);
-
-  // Create red garden
-  for (let i = 0; i < redGardenSize; i++) {
-    redGarden[i] = createFlower(random(0, width), random(0, height));
-  }
+  createCanvas(400, 300, WEBGL);
 }
 
-function createFlower(x, y) {
-  let redFlower = {
-    x: x,
-    y: y,
-    size: 30,
-    petalThickness: 10,
-    // Color information
-    petalColor: {
-      r: 255,
-      g: 50,
-      b: 50
-    },
-    centreColor: {
-      r: 50,
-      g: 0,
-      b: 0
-    }
-  };
-  return redFlower;
-}
+function draw() {
+  background(175);
 
-// draw()
-// Displays our flowers
-function draw(redFlower) {
-  background(0);
-  // Add flowers to garden
-  for (let i = 0; i < redGarden.length; i++) {
-    displayFlowers(redGarden[i]);
-  }
-}
-
-function mousePressed() {
-  let redFlower = createFlower(mouseX, mouseY);
-  redGarden.push(redFlower); // Add red flower to our array
-}
-
-function displayFlowers(redFlower) {
-  push();
-  strokeWeight(redFlower.petalThickness);
-  fill(
-    redFlower.centreColor.r,
-    redFlower.centreColor.g,
-    redFlower.centreColor.b
-  );
-  stroke(
-    redFlower.petalColor.r,
-    redFlower.petalColor.g,
-    redFlower.petalColor.b
-  );
-  ellipse(redFlower.x, redFlower.y, redFlower.size);
-  pop();
+  rectMode(CENTER);
+  rect(0, 0, 50, 50);
 }
