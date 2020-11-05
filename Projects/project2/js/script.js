4d61ea9423b67d2b78da9e571356e61ecccc6dcb
Atima <atima.ng@hotmail.ca>
2020-11-04 19:52:18 -0500

    P2: Started p2, uploaded pdf proposal

--- Projects/project2/js/script.js
+++ Projects/project2/js/script.js
@@ -1,42 +1,20 @@
-let rover;
+/**************************************************
+Template p5 project
+Pippin Barr
 
-//let angle = 0;
+Here is a description of this template p5 project.
+**************************************************/
 
+// setup()
+//
+// Description of setup() goes here.
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  rover = createRoverCam();
-  rover.usePointerLock();    // optional; default is keyboard control only
-  rover.setState({           // optional
-    position: [-500,-150,-200],
-    rotation: [0.4,0.3,0],
-    sensitivity: 0.1,
-    speed: 0.5
-  });
+  
 }
 
+// draw()
+//
+// Description of draw() goes here.
 function draw() {
-  background(120);
 
-  //noFill();
-  //stroke(0,250,0);
-  let dx = mouseX - width/2;
-  let dy = mouseY - height/2;
-  let v = createVector(dx,dy,0);
-  v.normalize();
-
-  ambientLight(0,0,255);
-  directionalLight(255,255,0,v);
-
-  //rotateY(angle);
-
-  //sphere(200);
-
-  //box(10,100,50);
-
-  //rectMode(CENTER);
-  //rect(0,0,300,300);
-  ambientMaterial(255);
-  plane(100,100,500,500);
-
-  //angle += 0.01;
-};
+}
