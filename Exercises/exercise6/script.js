73e1a80f5b79390f2954e7da5aa2bb33254bf899
Atima <atima.ng@hotmail.ca>
2020-11-12 17:54:40 -0500

    A6: Started activity 6

---
+++ Exercises/exercise6/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 06: Make Some Noise
+Atima Ng
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
